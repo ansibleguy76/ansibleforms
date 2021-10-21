9a818d0ef45a77c6ab1e50255a4b4b27cae6c944
ansibleguy76 <info@ansibleguy.com>
2021-10-21 16:29:35 +0200

    split login code to controller

--- server/src/routes/login.routes.js
+++ server/src/routes/login.routes.js
@@ -1,56 +1,6 @@
 const express = require('express')
-const passport = require('passport');
-const jwt = require('jsonwebtoken');
-var authConfig = require('../../config/auth.config.js')
-const logger=require("../lib/logger");
 const router = express.Router()
-const User = require("../models/user.model.js")
+const loginController = require("../controllers/login.controller.js")
 
-router.post(
-  '/login',
-  async (req, res, next) => {
-    passport.authenticate(
-      'login',
-      async (err, user, info) => {
-        try {
-          if (err || !user) {
-            var error={token:""}
-            if(err){
-              logger.debug(err)
-              error.message=err
-            }else{
-              logger.debug(info.message)
-              error.message=info.message;
-            }
-            return res.json(error);
-          }
-
-          req.login(
-            user,
-            { session: false },
-            async (error) => {
-              if (error) return next(error);
-              logger.debug(JSON.stringify(user))
-
-              const token = jwt.sign({user}, authConfig.secret,{ expiresIn: authConfig.jwtExpiration});
-              const refreshtoken = jwt.sign({user}, authConfig.secret,{ expiresIn: authConfig.jwtRefreshExpiration});
-              logger.debug("Storing refreshtoken in database for user " + user.username)
-              User.storeToken(user.username,user.type,refreshtoken,"AUTH",function(err,result){
-                if(err){
-                  logger.error(err)
-                }else{
-                  logger.debug(result)
-                }
-              })
-              authConfig.jwtStore[refreshtoken]=user.username
-              return res.json({ token,refreshtoken });
-            }
-          );
-        } catch (error) {
-          return next(error);
-        }
-      }
-    )(req, res, next);
-  }
-);
+router.post('/login',loginController.login);
 module.exports = router
