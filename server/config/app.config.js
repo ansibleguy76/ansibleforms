b598aade459c787c3f8b65bdfc57ab00c69fe0a1
ansibleguy76 <info@ansibleguy.com>
2021-11-17 17:51:18 +0100

    optimizing for docker

--- server/config/app.config.js
+++ server/config/app.config.js
@@ -2,7 +2,7 @@ const resolve = require("path").resolve
 const logger=require("../src/lib/logger");
 var app_config = {
   port: process.env.PORT || 8443,
-  nodeEnvironment: process.NODE_ENV || "development",
+  nodeEnvironment: process.NODE_ENV || "production",
   formsPath: process.env.FORMS_PATH || resolve("./../server/persistent/forms.json"),
   forceDotEnv: (process.env.FORCE_DOTENV==1) || false,
   encryptionSecret: ((process.env.ENCRYPTION_SECRET + "vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3").substring(0,32)) || "vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3"
--- server/config/https.config.js
+++ server/config/https.config.js
@@ -2,6 +2,6 @@ const fs=require('fs')
 const resolve=require('path').resolve
 module.exports = {
   https: (process.env.HTTPS=="1") || true,
-  httpsKey:fs.readFileSync(process.env.HTTPS_KEY) || resolve('../persistent/certificates/key.pem'),
-  httpsCert:fs.readFileSync(process.env.HTTPS_CERT) || resolve('../persistent/certificates/cert.pem')
+  httpsKey:fs.readFileSync(process.env.HTTPS_KEY || resolve('../persistent/certificates/key.pem')),
+  httpsCert:fs.readFileSync(process.env.HTTPS_CERT || resolve('../persistent/certificates/cert.pem'))
 };
