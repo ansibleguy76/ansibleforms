844fcc02477a30aa1c930a9e80fbee8f76bee747
ansibleguy76 <info@ansibleguy.com>
2021-11-17 20:02:19 +0100

    auto create logs directory

--- server/config/log.config.js
+++ server/config/log.config.js
@@ -1,7 +1,12 @@
+const fs = require("fs")
 var log_config = {
   level: process.env.LOG_LEVEL || "info",
   path: process.env.LOG_PATH || (__dirname + '/../persistent/logs/ansibleforms.log'),
   errorPath: process.env.LOG_ERRORPATH || (__dirname + '/../persistent/logs/ansibleforms.error.log'),
   consolelevel: process.env.LOG_CONSOLE_LEVEL || "info"
 };
+if ( !fs.existsSync( __dirname + '/../persistent/logs' ) ) {
+    // Create the directory if it does not exist
+    fs.mkdirSync( __dirname + '/../persistent/logs' );
+}
 module.exports = log_config
