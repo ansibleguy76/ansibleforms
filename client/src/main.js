0b76ac6fffa3c931ee94bdc6665a9e64105fc57f
ansibleguy76 <info@ansibleguy.com>
2021-11-28 23:33:13 +0100

    colorize toast

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Toast from "vue-toastification";
 // Import the CSS or use your own!
-import "vue-toastification/dist/index.css";
+import "./../public/assets/toast.scss";
 import { POSITION } from "vue-toastification";
 import App from './App.vue'
 import router from "./router"
