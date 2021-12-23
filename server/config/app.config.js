245081fb86a10d72e473f93391b5c27e3b1c86c8
WFAGuy.com <37864434+wfaguy@users.noreply.github.com>
2021-12-23 11:35:29 +0100

    env bug

--- server/config/app.config.js
+++ server/config/app.config.js
@@ -3,7 +3,6 @@ var app_config = {
   port: process.env.PORT || 8000,
   nodeEnvironment: process.env.NODE_ENV || "production",
   formsPath: process.env.FORMS_PATH || (__dirname + "/../persistent/forms.yaml"),
-  forceDotEnv: (process.env.FORCE_DOTENV==1) || false,
   encryptionSecret: ((process.env.ENCRYPTION_SECRET + "vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3").substring(0,32)) || "vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3"
 };
 module.exports = app_config;
