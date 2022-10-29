d132502652049dcb2620cda0b15c7c766eb09e13
vcmirko <37864434+wfaguy@users.noreply.github.com>
2022-10-29 22:26:07 +0200

    don't replace on numbers

--- client/src/components/Form.vue
+++ client/src/components/Form.vue
@@ -956,7 +956,7 @@
                 //fieldvalue=JSON.stringify(ref.form[foundfield])
                 // console.log(Helpers.replacePlaceholders(match[1],ref.form))
                 fieldvalue=JSON.stringify(Helpers.replacePlaceholders(match[1],ref.form)) // allow full object reference
-                if(item.dropQuotes){
+                if(item.dropQuotes && typeof fieldvalue=="string"){
                   fieldvalue=fieldvalue.replace(/^\"+/, '').replace(/\"+$/, ''); // eslint-disable-line
                 }
               }else{
