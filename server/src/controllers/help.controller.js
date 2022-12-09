e69ece85755d3e4b370be24a0ddc2cd9f1257959
vcmirko <37864434+wfaguy@users.noreply.github.com>
2022-12-09 09:45:10 +0100

    new help feature

---
+++ server/src/controllers/help.controller.js
@@ -0,0 +1,11 @@
+'use strict';
+var RestResult = require('../models/restResult.model');
+const Help = require('../models/help.model');
+const inspect = require("util").inspect
+
+
+exports.get = function(req, res) {
+    Help.get()
+      .then((help)=>{ res.json(new RestResult("success","help found",help,"")) })
+      .catch((err)=>{ res.json(new RestResult("error","failed to find help",null,err)) })
+};
