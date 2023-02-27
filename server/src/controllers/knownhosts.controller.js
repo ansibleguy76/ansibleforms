5ad959e65e372d32693e989271eab26f6c2dd15d
vcmirko <37864434+wfaguy@users.noreply.github.com>
2023-02-27 21:05:17 +0100

    adding settings for knownhosts

---
+++ server/src/controllers/knownhosts.controller.js
@@ -0,0 +1,29 @@
+'use strict';
+const KnownHosts = require('../models/knownhosts.model');
+var RestResult = require('../models/restResult.model');
+
+
+exports.find = function(req, res) {
+    KnownHosts.findAll()
+      .then((hosts)=>{res.json(new RestResult("success","Hosts found",hosts,""))})
+      .catch((err)=>{res.json(new RestResult("error","failed to find Hosts",null,err))})
+};
+exports.add = function(req, res) {
+    //handles null error
+    if(req.body.constructor === Object && Object.keys(req.body).length === 0){
+        res.status(400).send({ error:true, message: 'Please provide all required fields' });
+    }else{
+        KnownHosts.add(req.body.host)
+          .then((output)=>{res.json(new RestResult("success","Host added",output,""))})
+          .catch((err)=>{res.json(new RestResult("error","failed to add host",null,err))})
+    }
+};
+exports.remove = function(req, res) {
+  if(req.query.name){
+    KnownHosts.remove( req.query.name)
+      .then(()=>{res.json(new RestResult("success","Host removed",null,""))})
+      .catch((err)=>{res.json(new RestResult("error","failed to remove host",null,err))})
+  }else{
+    res.json(new RestResult("error","no host name specified",null,""));
+  }
+};
