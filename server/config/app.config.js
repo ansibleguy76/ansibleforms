ad360c10edd41df54a392c5f7aef2fc49eb19ca5
vcmirko <37864434+wfaguy@users.noreply.github.com>
2023-10-07 10:16:07 +0200

    move to ubuntu

--- server/config/app.config.js
+++ server/config/app.config.js
@@ -6,7 +6,7 @@ var app_config = {
   showDesigner: process.env.SHOW_DESIGNER ?? true,
   formsPath: process.env.FORMS_PATH || path.resolve(__dirname + "/../persistent/forms.yaml"),
   lockPath: process.env.LOCK_PATH || path.resolve(__dirname + "/../persistent/ansibleForms.lock"),
-  helpPath: path.resolve(__dirname + "/../help.yaml"),
+  helpPath: path.resolve(__dirname + "/../../docs/_data/help.yaml"),
   encryptionSecret: ((process.env.ENCRYPTION_SECRET + "vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3").substring(0,32)) || "vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3",
   homePath: process.env.HOME_PATH || require('os').homedir(),
   uploadPath: process.env.UPLOAD_PATH || path.resolve(__dirname + "/../persistent/uploads"),
