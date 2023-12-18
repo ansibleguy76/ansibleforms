3ad6e1ecc71c5c8f9a59a6047a3efbaa88e1f400
Mirko Van Colen <mirko@vancolen.com>
2023-12-18 10:12:52 +0100

    add repositories

--- server/src/init/index.js
+++ server/src/init/index.js
@@ -2,7 +2,10 @@ const logger=require("../lib/logger");
 var Ssh = require('../models/ssh.model');
 var Form = require('../models/form.model');
 var Job = require('../models/job.model');
-
+const mysql=require("../models/db.model");
+const Repository = require('../models/repository.model');
+const parser = require("cron-parser")
+const dayjs = require("dayjs")
 
 Ssh.generate(false)
   .catch((err)=>{
@@ -30,3 +33,39 @@ setInterval(()=>{
       logger.error("Failed to abandon jobs : " + err)
     })
 },3600000)
+
+logger.info("Pulling repositories")
+mysql.do("SELECT name FROM AnsibleForms.`repositories` WHERE rebase_on_start=1")
+.then((repositories)=>{
+  repositories.map((repo)=>{
+    logger.info("Pulling " + repo.name)
+    Repository.clone(repo.name).catch((e)=>{})
+  })
+})
+.catch((e)=>{})
+
+logger.info("Initializing repository cron schedules")
+// this is hourly, abandon running jobs older than a day.
+setInterval(()=>{
+  mysql.do("SELECT name,cron FROM AnsibleForms.`repositories` WHERE status<>'running' AND cron<>''",undefined,true)
+  .then((repositories)=>{
+    repositories.map((repo)=>{
+      try{
+        const interval = parser.parseExpression(repo.cron) 
+        const next = interval.next().toDate()
+        const date = dayjs(next)
+        const now = dayjs()
+        const minutes = date.diff(now,'m')
+        if(minutes==0){
+          Repository.pull(repo.name)
+        }else{
+          // logger.debug(`Not time yet, ${minutes} minutes to go`)
+        }
+      }catch(e){
+        logger.error(`Failed to parse cron schedule ${repo.cron}`)
+      }       
+
+    })
+  })
+  .catch((e)=>{})  
+},56000) // run every 55 second, should hit 0 minutes once
