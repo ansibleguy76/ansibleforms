bf62c3d5552f550750078f43e0dfff73dc4b8667
Mirko Van Colen <mirko@vancolen.com>
2023-12-18 10:10:40 +0100

    add repositories

---
+++ server/src/db/create_repositories_table.sql
@@ -0,0 +1,18 @@
+USE `AnsibleForms`;
+CREATE TABLE `repositories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) NOT NULL,
+  `user` varchar(250) DEFAULT NULL,
+  `password` text DEFAULT NULL,
+  `uri` varchar(250) DEFAULT NULL,
+  `description` text NOT NULL,
+  `use_for_forms` tinyint(4) DEFAULT NULL,
+  `use_for_playbooks` tinyint(4) DEFAULT NULL,  
+  `cron` varchar(50) DEFAULT NULL,  
+  `status` varchar(50) DEFAULT NULL,
+  `output` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `head` varchar(50) DEFAULT NULL,    
+  `rebase_on_start` tinyint(4) DEFAULT NULL,  
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_AnsibleForms_repositories_natural_key` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
