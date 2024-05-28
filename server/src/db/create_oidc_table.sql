c42079b812273bdee2799f40bab9061b48f140b3
Markus Daugs <markus.daugs@cgi.com>
2024-05-28 13:09:38 +0200

    add oidc table

---
+++ server/src/db/create_oidc_table.sql
@@ -0,0 +1,10 @@
+USE `AnsibleForms`;
+DROP TABLE IF EXISTS `oidc`;
+CREATE TABLE `oidc` (
+  `issuer` text DEFAULT NULL,
+  `client_id` text DEFAULT NULL,
+  `secret_id` text DEFAULT NULL,
+  `enable` tinyint(4) DEFAULT NULL,
+  `groupfilter` varchar(250) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO AnsibleForms.oidc(issuer, client_id,secret_id,enable,groupfilter) VALUES('', '','',0,'');
