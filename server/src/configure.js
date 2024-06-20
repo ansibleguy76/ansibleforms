d8f25f1847e22e378b8358b287633a1de70c0eaa
Markus Daugs <markus.daugs@cgi.com>
2024-06-20 13:01:46 +0200

    express router config in async context does not work properly

--- server/src/configure.js
+++ server/src/configure.js
@@ -25,6 +25,43 @@ const checkAdminMiddleware = require('./lib/common').checkAdminMiddleware
 
 // our personal app settings
 const appConfig = require('../config/app.config')
+const logger = require("./lib/logger");
+const schemaRoutes = require("./routes/schema.routes");
+const queryRoutes = require("./routes/query.routes");
+const expressionRoutes = require("./routes/expression.routes");
+const versionRoutes = require("./routes/version.routes");
+const installRoutes = require("./routes/install.routes");
+const lockRoutes = require("./routes/lock.routes");
+const helpRoutes = require("./routes/help.routes");
+const profileRoutes = require("./routes/profile.routes");
+const loginRoutes = require("./routes/login.routes");
+const tokenRoutes = require("./routes/token.routes");
+const jobRoutes = require("./routes/job.routes");
+const userRoutes = require("./routes/user.routes");
+const groupRoutes = require("./routes/group.routes");
+const ldapRoutes = require("./routes/ldap.routes");
+const azureadRoutes = require("./routes/azuread.routes");
+const oidcRoutes = require("./routes/oidc.routes");
+const settingsRoutes = require("./routes/settings.routes");
+const credentialRoutes = require("./routes/credential.routes");
+const sshRoutes = require("./routes/ssh.routes");
+const awxRoutes = require("./routes/awx.routes");
+const logRoutes = require("./routes/log.routes");
+const repositoryRoutes = require("./routes/repository.routes");
+const knownhostsRoutes = require("./routes/knownhosts.routes");
+const configRoutes = require("./routes/config.routes");
+
+// we use 4 authentications/authorization strategies
+// - basic : with username and password to get jwt tokens
+// - azure-ad-oauth2 : microsoft login
+// - oidc : open id connect
+// - jwt : to use the jwt tokens
+// passport (the auth lib used) is smart, if basic authentication headers are detected
+// then the basic authentication strategy kicks and the basic login procedure starts
+require('./auth/auth_basic');
+require('./auth/auth_jwt');
+const auth_azuread = require('./auth/auth_azuread');
+const auth_oidc = require('./auth/auth_oidc');
 
 // start the app
 module.exports = app => {
@@ -32,126 +69,88 @@ module.exports = app => {
   // first time run init
   // from now on, it's async => we wait for mysql to be ready
   const init = require('./init/')
-  init().then(()=>{
+  init()
+      .then(()=> {
+        auth_azuread.initialize()
+        auth_oidc.initialize()
+      })
+      .catch(
+          r => logger.error(r)
+      );
 
-    // passport
-    app.use(session({ 
-      secret: 'AnsibleForms',
-      resave: false,
-      saveUninitialized: true
-    }));
-    app.use(passport.initialize());
-    app.use(passport.session());
+  // passport
+  app.use(session({
+    secret: 'AnsibleForms',
+    resave: false,
+    saveUninitialized: true
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
 
-    // we use 4 authentications/authorization strategies
-    // - basic : with username and password to get jwt tokens
-    // - azure-ad-oauth2 : microsoft login
-    // - oidc : open id connect
-    // - jwt : to use the jwt tokens
-    // passport (the auth lib used) is smart, if basic authentication headers are detected
-    // then the basic authentication strategy kicks and the basic login procedure starts
-    require('./auth/auth_basic');
-    require('./auth/auth_jwt');
-    const auth_azuread = require('./auth/auth_azuread');  
-    auth_azuread.initialize()
+  app.use(bodyParser.json({limit: '50mb'}));
+  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
-    const auth_oidc = require('./auth/auth_oidc');
-    auth_oidc.initialize()
-
-    app.use(bodyParser.json({limit: '50mb'}));
-    app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-
-    // import api routes
-    const awxRoutes = require('./routes/awx.routes')
-    const jobRoutes = require('./routes/job.routes')
-    const queryRoutes = require('./routes/query.routes')
-    const expressionRoutes = require('./routes/expression.routes')
-    const userRoutes = require('./routes/user.routes')
-    const groupRoutes = require('./routes/group.routes')
-    const ldapRoutes = require('./routes/ldap.routes')
-    const azureadRoutes = require('./routes/azuread.routes')
-    const oidcRoutes = require('./routes/oidc.routes')
-    const settingsRoutes = require('./routes/settings.routes')
-    const credentialRoutes = require('./routes/credential.routes')
-    const loginRoutes = require('./routes/login.routes')
-    const schemaRoutes = require('./routes/schema.routes')
-    const tokenRoutes = require('./routes/token.routes')
-    const configRoutes = require('./routes/config.routes')
-    const versionRoutes = require('./routes/version.routes')
-    const lockRoutes = require('./routes/lock.routes')
-    const profileRoutes = require('./routes/profile.routes')
-    const sshRoutes = require('./routes/ssh.routes')
-    const logRoutes = require('./routes/log.routes')
-    const knownhostsRoutes = require('./routes/knownhosts.routes')
-    const helpRoutes = require('./routes/help.routes')
-    const installRoutes = require('./routes/install.routes')
-    const repositoryRoutes = require('./routes/repository.routes')
-
-    // mysql2 has a bug that can throw an uncaught exception if the mysql server crashes (not enough mem for example)
-    // also git commands can chain child processes and cause issues
-    process.on('uncaughtException', function(err) {
-      // handle the error safely
-      console.error("An uncaught exception happened, ignore... ",err)
-    })
-
-    // using json web tokens as middleware
-    // the jwtauthentication strategy from passport (/auth/auth.js)
-    // is used as a middleware.  Every route will check the token for validity
-    const authobj = passport.authenticate('jwt', { session: false })
-
-    // api routes for browser only (no cors)
-    const swaggerOptions = {
-      customSiteTitle: "Ansibleforms Swagger UI",
-      customfavIcon: `${appConfig.baseUrl}favicon.svg`,
-      customCssUrl: `${appConfig.baseUrl}assets/css/swagger.css`,
-      docExpansion:"none"
-    }
-    // change basePath dynamically
-    swaggerDocument.basePath = `${appConfig.baseUrl}api/v1`
-    app.use(`${appConfig.baseUrl}api-docs`, swaggerUi.serve, swaggerUi.setup(swaggerDocument,swaggerOptions));
-    app.use(`${appConfig.baseUrl}api/v1/schema`, schemaRoutes)
-
-    // api routes for querying
-    app.use(`${appConfig.baseUrl}api/v1/query`,cors(), authobj, queryRoutes)
-    app.use(`${appConfig.baseUrl}api/v1/expression`,cors(), authobj, expressionRoutes)
-
-    // api route for version
-    app.use(`${appConfig.baseUrl}api/v1/version`,cors(), versionRoutes)
-    app.use(`${appConfig.baseUrl}api/v1/install`,cors(), installRoutes)
-
-    app.use(`${appConfig.baseUrl}api/v1/lock`,cors(),authobj, lockRoutes)
-    app.use(`${appConfig.baseUrl}api/v1/help`,cors(),authobj, helpRoutes)    
-
-    // api route for profile
-    app.use(`${appConfig.baseUrl}api/v1/profile`,cors(), authobj, profileRoutes)
-
-    // api routes for authorization
-    app.use(`${appConfig.baseUrl}api/v1/auth`,cors(), loginRoutes)
-    app.use(`${appConfig.baseUrl}api/v1/token`,cors(), tokenRoutes)
-
-    // api routes for automation actions
-
-    // app.use(`${appConfig.baseUrl}api/v1/multistep`,cors(), authobj, multistepRoutes)
-
-    // api routes for admin management
-    app.use(`${appConfig.baseUrl}api/v1/job`,cors(), authobj, jobRoutes)
-    app.use(`${appConfig.baseUrl}api/v1/user`,cors(), authobj, checkAdminMiddleware, userRoutes)
-    app.use(`${appConfig.baseUrl}api/v1/group`,cors(), authobj, checkAdminMiddleware, groupRoutes)
-    app.use(`${appConfig.baseUrl}api/v1/ldap`,cors(), authobj, checkAdminMiddleware, ldapRoutes)
-    app.use(`${appConfig.baseUrl}api/v1/azuread`,cors(), authobj, checkAdminMiddleware, azureadRoutes)
-    app.use(`${appConfig.baseUrl}api/v1/oidc`,cors(), authobj, checkAdminMiddleware, oidcRoutes)
-    app.use(`${appConfig.baseUrl}api/v1/settings`,cors(), authobj, checkAdminMiddleware, settingsRoutes)
-    app.use(`${appConfig.baseUrl}api/v1/credential`,cors(), authobj, checkAdminMiddleware, credentialRoutes)
-    app.use(`${appConfig.baseUrl}api/v1/sshkey`,cors(), authobj, checkAdminMiddleware, sshRoutes)
-    app.use(`${appConfig.baseUrl}api/v1/awx`,cors(), authobj, checkAdminMiddleware, awxRoutes)
-    app.use(`${appConfig.baseUrl}api/v1/log`,cors(), authobj, checkAdminMiddleware, logRoutes)
-    app.use(`${appConfig.baseUrl}api/v1/repository`,cors(), authobj, checkAdminMiddleware, repositoryRoutes)
-    app.use(`${appConfig.baseUrl}api/v1/knownhosts`,cors(), authobj, checkAdminMiddleware, knownhostsRoutes)
-
-    // routes for form config (extra middleware in the routes itself)
-    app.use(`${appConfig.baseUrl}api/v1/config`,cors(), authobj, configRoutes)
-
+  // mysql2 has a bug that can throw an uncaught exception if the mysql server crashes (not enough mem for example)
+  // also git commands can chain child processes and cause issues
+  process.on('uncaughtException', function(err) {
+    // handle the error safely
+    console.error("An uncaught exception happened, ignore... ",err)
   })
 
- 
+  // using json web tokens as middleware
+  // the jwtauthentication strategy from passport (/auth/auth.js)
+  // is used as a middleware.  Every route will check the token for validity
+  const authobj = passport.authenticate('jwt', { session: false })
+
+  // api routes for browser only (no cors)
+  const swaggerOptions = {
+    customSiteTitle: "Ansibleforms Swagger UI",
+    customfavIcon: `${appConfig.baseUrl}favicon.svg`,
+    customCssUrl: `${appConfig.baseUrl}assets/css/swagger.css`,
+    docExpansion:"none"
+  }
+  // change basePath dynamically
+  swaggerDocument.basePath = `${appConfig.baseUrl}api/v1`
+  app.use(`${appConfig.baseUrl}api-docs`, swaggerUi.serve, swaggerUi.setup(swaggerDocument,swaggerOptions));
+  app.use(`${appConfig.baseUrl}api/v1/schema`, schemaRoutes)
+
+  // api routes for querying
+  app.use(`${appConfig.baseUrl}api/v1/query`,cors(), authobj, queryRoutes)
+  app.use(`${appConfig.baseUrl}api/v1/expression`,cors(), authobj, expressionRoutes)
+
+  // api route for version
+  app.use(`${appConfig.baseUrl}api/v1/version`,cors(), versionRoutes)
+  app.use(`${appConfig.baseUrl}api/v1/install`,cors(), installRoutes)
+
+  app.use(`${appConfig.baseUrl}api/v1/lock`,cors(),authobj, lockRoutes)
+  app.use(`${appConfig.baseUrl}api/v1/help`,cors(),authobj, helpRoutes)
+
+  // api route for profile
+  app.use(`${appConfig.baseUrl}api/v1/profile`,cors(), authobj, profileRoutes)
+
+  // api routes for authorization
+  app.use(`${appConfig.baseUrl}api/v1/auth`,cors(), loginRoutes)
+  app.use(`${appConfig.baseUrl}api/v1/token`,cors(), tokenRoutes)
+
+  // api routes for automation actions
+
+  // app.use(`${appConfig.baseUrl}api/v1/multistep`,cors(), authobj, multistepRoutes)
+
+  // api routes for admin management
+  app.use(`${appConfig.baseUrl}api/v1/job`,cors(), authobj, jobRoutes)
+  app.use(`${appConfig.baseUrl}api/v1/user`,cors(), authobj, checkAdminMiddleware, userRoutes)
+  app.use(`${appConfig.baseUrl}api/v1/group`,cors(), authobj, checkAdminMiddleware, groupRoutes)
+  app.use(`${appConfig.baseUrl}api/v1/ldap`,cors(), authobj, checkAdminMiddleware, ldapRoutes)
+  app.use(`${appConfig.baseUrl}api/v1/azuread`,cors(), authobj, checkAdminMiddleware, azureadRoutes)
+  app.use(`${appConfig.baseUrl}api/v1/oidc`,cors(), authobj, checkAdminMiddleware, oidcRoutes)
+  app.use(`${appConfig.baseUrl}api/v1/settings`,cors(), authobj, checkAdminMiddleware, settingsRoutes)
+  app.use(`${appConfig.baseUrl}api/v1/credential`,cors(), authobj, checkAdminMiddleware, credentialRoutes)
+  app.use(`${appConfig.baseUrl}api/v1/sshkey`,cors(), authobj, checkAdminMiddleware, sshRoutes)
+  app.use(`${appConfig.baseUrl}api/v1/awx`,cors(), authobj, checkAdminMiddleware, awxRoutes)
+  app.use(`${appConfig.baseUrl}api/v1/log`,cors(), authobj, checkAdminMiddleware, logRoutes)
+  app.use(`${appConfig.baseUrl}api/v1/repository`,cors(), authobj, checkAdminMiddleware, repositoryRoutes)
+  app.use(`${appConfig.baseUrl}api/v1/knownhosts`,cors(), authobj, checkAdminMiddleware, knownhostsRoutes)
+
+  // routes for form config (extra middleware in the routes itself)
+  app.use(`${appConfig.baseUrl}api/v1/config`,cors(), authobj, configRoutes)
 }
