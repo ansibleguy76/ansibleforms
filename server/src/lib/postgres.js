1f406f808be37e1ba4e668a52256e5f1a95b27ba
Mirko Van Colen <mirko@vancolen.com>
2024-06-20 17:02:43 +0200

    rewritten in async await

--- server/src/lib/postgres.js
+++ server/src/lib/postgres.js
@@ -5,41 +5,34 @@ const Credential = require('../models/credential.model')
 
 Postgres = {}
 
-Postgres.query=function(connection_name,query){
+// rewritten with await 5.0.3
+Postgres.query = async function (connection_name, query) {
+  var creds = await Credential.findByName(connection_name)
+  var config = {
+      host: creds.host,
+      user: creds.user,
+      password: creds.password,
+      database: creds.db_name||creds.user,
+      port: creds.port,
+  };
+  var client
+  try{
+    client = new Client(config)
+    await client.connect()
+    var result = await client.query(query)
+    return result?.rows
+  }catch(err){
+    logger.error(`[${connection_name}] query error`,err)
+    throw err
+  }finally{
+    try{
+      // logger.debug("["+connection_name+"] Closing connection")
+      await client.end()
+    }catch(e){
+      logger.error(`[${connection_name}] connection error`,e)
+      throw e
+    }
+  }
+}
 
-  return Credential.findByName(connection_name)
-  .then((creds)=>{
-    var config = {
-        host: creds.host,
-        user: creds.user,
-        password: creds.password,
-        database: creds.db_name||creds.user,
-        port: creds.port,
-    };
-    // console.log(config)
-    return config
-  })  
-  .then((config)=>{
-    logger.debug(`[${connection_name}] query : ${query}`)
-    return new Promise((resolve,reject)=>{
-      var client
-      try{
-        client = new Client(config)
-        return client.connect()
-        .then(() => client.query(query))
-        .catch((err)=>{ reject(`[${connection_name}] connection error : ${err.toString()}`) })
-        .then((result)=>{
-            logger.debug("["+connection_name+"] Closing connection")
-            client.end()
-            resolve(result?.rows)
-        }).catch((err)=>{
-          reject(`[${connection_name}] query error : ${err.toString()}`)
-        })
-      }catch(err){
-        reject(`[${connection_name}] connection error : ${err.toString()}`)
-      }
-    })
-
-  })
-};
 module.exports = Postgres
