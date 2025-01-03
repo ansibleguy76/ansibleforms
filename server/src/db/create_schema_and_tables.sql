b8a138893340066771af79c20b4d5b165104d48c
ansibleguy76 <info@ansibleguy.com>
2025-01-03 19:14:38 +0100

    remove user create

--- server/src/db/create_schema_and_tables.sql
+++ server/src/db/create_schema_and_tables.sql
@@ -169,7 +169,6 @@ INSERT INTO AnsibleForms.azuread(client_id,secret_id,enable) VALUES('','',0);
 INSERT INTO AnsibleForms.oidc(issuer, client_id,secret_id,enabled,groupfilter) VALUES('', '','',0,'');
 INSERT INTO AnsibleForms.groups(name) VALUES('admins');
 INSERT INTO AnsibleForms.awx(uri,token,username,password,ignore_certs,use_credentials,ca_bundle) VALUES('','','','',0,0,'');
-INSERT INTO AnsibleForms.users(username,password,email,group_id) VALUES('admin','$2b$10$Z/W0HXNBk2aLR4yVLkq5L..C8tXg.G.o1vkFr8D2lw8JSgWRCNiCa','',1);
 INSERT INTO AnsibleForms.ldap(server,port,ignore_certs,enable_tls,cert,ca_bundle,bind_user_dn,bind_user_pw,search_base,username_attribute,enable) VALUES('',389,1,0,'','','','','','sAMAccountName',0);
 INSERT INTO AnsibleForms.settings(mail_server,mail_port,mail_secure,mail_username,mail_password,mail_from,url,forms_yaml) VALUES('',25,0,'','','','','');
 
