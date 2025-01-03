243f06cafd4e196fc06504197ae083645f61895a
ansibleguy76 <info@ansibleguy.com>
2025-01-03 18:29:57 +0100

    remove log

--- client/src/lib/TokenStorage.js
+++ client/src/lib/TokenStorage.js
@@ -28,8 +28,10 @@ var TokenStorage = {
       jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
           return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
       }).join(''));
+      // console.log(JSON.parse(jsonPayload))
       return JSON.parse(jsonPayload)
     }catch(err){
+      console.log("Error in getPayload: " + err)
       return {}
     }
   },
