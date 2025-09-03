f3cc7d8673802b7732db2391084f8b2af84551fe
ansibleguy76 <info@ansibleguy.com>
2025-09-03 15:22:25 +0200

    add new schema in v2 api

--- server/src/controllers/schema.controller.js
+++ server/src/controllers/schema.controller.js
@@ -1,31 +1,31 @@
 'use strict';
 import Schema from '../models/schema.model.js';
-import RestResult from '../models/restResult.model.js';
-import helpers from '../lib/common.js';
+import RestResult from '../models/restResult.model.v2.js';
+import errors from '../lib/errors.js';
 
-const hasSchema = function(req, res) {
+const schemaController = {
+  async hasSchema(req, res) {
+    try {
+      const result = await Schema.hasSchema();
+      if(!result?.data){
+        errors.ReturnError(res, new Error(`FATAL ERROR : ${result.message}`));
+      }
+      res.json(RestResult.single(result.data))
+    } catch (err) {
+      errors.ReturnError(res, err);
+    }
 
-    Schema.hasSchema()
-      .then((result)=>{ res.json(new RestResult("success","schema and tables are ok",result.data?.success,result.data?.failed)) })
-      .catch((err)=>{ 
-        var result = err.result // we check if the error has a result
-        if(!result?.data){
-          res.json(new RestResult("error","FATAL ERROR",[],[result.message])) 
-        }else{
-          res.json(new RestResult("error","schema and tables are not ok",result.data?.success,result.data?.failed)) 
-        }
-      })
+  },
 
-};
-const create = function(req, res) {
-    Schema.create()
-      .then((result)=>{ res.json(new RestResult("success",result,null,"")) })
-      .catch((err)=>{
-        res.json(new RestResult("error",helpers.getError(err),null,null))
-      })
-};
+  async create(req, res) {
+    try {
+      const result = await Schema.create();
+      res.json(RestResult.single(result));
+    } catch (err) {
+      errors.ReturnError(res, err);
+    }
+  }
+}
 
-export default {
-  hasSchema,
-  create
-};
+
+export default schemaController;
--- server/src/models/schema.model.js
+++ server/src/models/schema.model.js
@@ -1,3 +1,29 @@
+// PATCHING : Drop a table from the schema
+function dropTable(table) {
+  var message;
+  var db = "AnsibleForms";
+  var checksql = `SHOW TABLES FROM ${db} WHERE Tables_in_${db}='${table}'`;
+  var sql = `DROP TABLE IF EXISTS ${db}.${table}`;
+  logger.debug(`dropping table '${table}'`);
+  return mysql
+    .do(checksql)
+    .then((checkres) => {
+      if (checkres.length > 0) {
+        return mysql.do(sql);
+      }
+      return false;
+    })
+    .then((res) => {
+      if (!res) {
+        message = `Table '${table}' is already absent`;
+        logger.debug(message);
+        return message;
+      }
+      message = `Dropped table '${table}'`;
+      logger.warning(message);
+      return message;
+    });
+}
 "use strict";
 import logger from "../lib/logger.js";
 import mysql from "./db.model.js";
@@ -5,12 +31,37 @@ import appConfig from "../../config/app.config.js";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+import init from "../init/index.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 //user object create
-var Schema = function () {};
+class Schema {
+  constructor() { }
+  static async hasSchema() {
+    return await checkAll();
+  }
+  static async create() {
+    logger.notice(`Trying to create database schema 'AnsibleForms' and tables`);
+    if (appConfig.allowSchemaCreation) {
+      // added in 5.0.3
+      const buffer = fs.readFileSync(`${__dirname}/../db/create_schema_and_tables.sql`);
+      const query = buffer.toString();
+      var res = await mysql.do(query);
+      if (res.length > 0) {
+        logger.notice(`Created schema 'AnsibleForms' and tables`);
+        await init()
+        return { message: `Created schema 'AnsibleForms' and tables` };
+      } else {
+        throw new Error(`Failed to create schema 'AnsibleForms' and/or tables`);
+      }
+    } else {
+      throw new Error(`Schema creation is disabled`);
+    }
+    
+  }
+}
 
 // Check if the AnsibleForms schema exists
 async function checkSchema() {
@@ -225,14 +276,14 @@ async function patchVersion5(messages, success, failed) {
   // AF passes the user object and the email address can then be used in ansible for notifications
   await checkPromise(addColumn("users", "email", "varchar(250)", true, "NULL"), messages, success, failed); // add column to have email
 
-  // Azure AD integration was added in 5.0.0, so we need to add the table and columns
-  buffer = fs.readFileSync(`${__dirname}/../db/create_azuread_table.sql`);
-  sql = buffer.toString();
-  await checkPromise(addTable("azuread", sql), messages, success, failed); // add azuread table
+  // // Azure AD integration was added in 5.0.0, so we need to add the table and columns
+  // buffer = fs.readFileSync(`${__dirname}/../db/create_azuread_table.sql`);
+  // sql = buffer.toString();
+  // await checkPromise(addTable("azuread", sql), messages, success, failed); // add azuread table
 
-  // A bit later, the groupfilter was added to limit the groups that can be used in AF, because the list can be very long and that's part of the JWT token
-  // which introduced a limit
-  await checkPromise(addColumn("azuread", "groupfilter", "varchar(250)", true, "NULL"), messages, success, failed); // add column to limit azuread groups
+  // // A bit later, the groupfilter was added to limit the groups that can be used in AF, because the list can be very long and that's part of the JWT token
+  // // which introduced a limit
+  // await checkPromise(addColumn("azuread", "groupfilter", "varchar(250)", true, "NULL"), messages, success, failed); // add column to limit azuread groups
 
   // on request, the awx_id was added to the jobs table, to later retrieve it for future tracking
   await checkPromise(addColumn("jobs", "awx_id", "int(11)", true, "NULL"), messages, success, failed); // add for future tracking
@@ -246,10 +297,10 @@ async function patchVersion5(messages, success, failed) {
   sql = buffer.toString();
   await checkPromise(addTable("repositories", sql), messages, success, failed); // add repositories table
 
-  // In a first pull requests, mdaug was so kind to contribute the oidc table, to have OpenID Connect integration
-  buffer = fs.readFileSync(`${__dirname}/../db/create_oidc_table.sql`);
-  sql = buffer.toString();
-  await checkPromise(addTable("oidc", sql), messages, success, failed); // add oidc table
+  // // In a first pull requests, mdaug was so kind to contribute the oidc table, to have OpenID Connect integration
+  // buffer = fs.readFileSync(`${__dirname}/../db/create_oidc_table.sql`);
+  // sql = buffer.toString();
+  // await checkPromise(addTable("oidc", sql), messages, success, failed); // add oidc table
 
   // In 5.0.3, the settings was extended with a new column, forms_yaml, to store the forms in yaml format
   // If you use GIT to store the forms, but not the master forms.yaml, you can now store it in the database
@@ -284,19 +335,71 @@ async function patchVersion5(messages, success, failed) {
   await checkPromise(addColumn("awx", "is_default", "tinyint(4)", true, "0"), messages, success, failed); // add is_default column
 }
 
+// Patches for v6 
+// In 6.0.0 we add oauth2 providers table
+async function patchVersion6(messages, success, failed) {
+  var buffer;
+  var sql;
+  // In 6.0.0, we add oauth2 providers table
+  buffer = fs.readFileSync(`${__dirname}/../db/create_oauth2_providers_table.sql`);
+  sql = buffer.toString();
+  await checkPromise(addTable("oauth2_providers", sql), messages, success, failed);
+
+  // --- AzureAD migration to oauth2_providers ---
+  // Check if azuread table exists
+  try {
+    // AzureAD migration
+    const azureadTableCheck = await mysql.do("SHOW TABLES FROM AnsibleForms WHERE Tables_in_AnsibleForms = 'azuread'");
+    if (azureadTableCheck.length > 0) {
+      // Table exists, migrate record(s) from azuread to oauth2_providers if client_id is not empty
+      const insertSql = `
+        INSERT INTO AnsibleForms.oauth2_providers (name, provider, client_id, client_secret, groupfilter, enable)
+        SELECT 'EntraID', 'azuread', client_id, secret_id, groupfilter, enable
+        FROM AnsibleForms.azuread
+        WHERE client_id IS NOT NULL AND client_id != ''
+      `;
+      await mysql.do(insertSql)
+    }
+
+    // OIDC migration
+    const oidcTableCheck = await mysql.do("SHOW TABLES FROM AnsibleForms WHERE Tables_in_AnsibleForms = 'oidc'");
+    if (oidcTableCheck.length > 0) {
+      // Table exists, migrate record(s) from oidc to oauth2_providers if client_id is not empty
+      const insertOidcSql = `
+        INSERT INTO AnsibleForms.oauth2_providers (name, provider, client_id, client_secret, groupfilter, enable, issuer)
+        SELECT 'OpenID', 'oidc', client_id, secret_id, groupfilter, enabled, issuer
+        FROM AnsibleForms.oidc
+        WHERE client_id IS NOT NULL AND client_id != ''
+      `;
+      await mysql.do(insertOidcSql)
+    }
+  } catch (e) {
+    // messages.push("AzureAD/OIDC migration failed: " + e.message);
+    // failed.push("AzureAD/OIDC migration failed: " + e.message);
+  }
+  // Drop the azuread table using dropTable and checkPromise
+  await checkPromise(dropTable("azuread"), messages, success, failed);
+  await checkPromise(dropTable("oidc"), messages, success, failed);
+}
+
 // PATCHING : Patch All
 async function patchAll(messages, success, failed) {
   await checkPromise(patchVersion4(messages, success, failed), messages, success, failed);
   await checkPromise(patchVersion5(messages, success, failed), messages, success, failed);
+  await checkPromise(patchVersion6(messages, success, failed), messages, success, failed);
 }
 async function checkPromise(promise, messages, success, failed) {
   try {
     var res = await promise;
-    messages.push(res);
-    success.push(res);
+    if(res!=null){
+      messages.push(res);
+      success.push(res);
+    }
   } catch (e) {
-    messages.push(e.message);
-    failed.push(e.message);
+    if(e.message!=null){
+      messages.push(e.message);
+      failed.push(e.message);
+    }
   }
 }
 
@@ -318,7 +421,7 @@ async function checkAll() {
   if (failed.length > 0) {
     // if schema does not exist, we can't check the tables // throw now
     resultobj = { message: messages, data: { success: success, failed: failed } };
-    var err = new Error(messages.toString());
+    var err = new Error("Schema is missing");
     err.result = resultobj;
     throw err;
   }
@@ -330,7 +433,7 @@ async function checkAll() {
   if (failed.length > 0) {
     // some of the tables are missing, we can't patch them // throw now
     resultobj = { message: messages, data: { success: success, failed: failed } };
-    var err = new Error(messages.toString());
+    var err = new Error("Tables are missing");
     err.result = resultobj;
     throw err;
   }
@@ -341,7 +444,7 @@ async function checkAll() {
   if (failed.length > 0) {
     // some of the patches failed // throw now
     resultobj = { message: messages, data: { success: success, failed: failed } };
-    var err = new Error(messages.toString());
+    var err = new Error("Patching failed");
     err.result = resultobj;
     throw err;
   }
@@ -350,27 +453,6 @@ async function checkAll() {
   resultobj = { message: messages, data: { success: success, failed: failed } };
   return resultobj;
 }
-Schema.hasSchema = function () {
-  return checkAll();
-};
-Schema.create = async function () {
-  logger.notice(`Trying to create database schema 'AnsibleForms' and tables`);
-  if (appConfig.allowSchemaCreation) {
-    // added in 5.0.3
-    const buffer = fs.readFileSync(`${__dirname}/../db/create_schema_and_tables.sql`);
-    const query = buffer.toString();
-    var res = await mysql.do(query);
-    if (res.length > 0) {
-      logger.notice(`Created schema 'AnsibleForms' and tables`);
-      return `Created schema 'AnsibleForms' and tables`;
-    } else {
-      var err = new Error(`Failed to create schema 'AnsibleForms' and/or tables`);
-      throw err;
-    }
-  } else {
-    throw new Error(`Schema creation is disabled`);
-  }
-};
 
 export default Schema;
 
