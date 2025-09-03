9d0b1f5d3230e3eb75eb8e239661e2cb8d8b7fd8
ansibleguy76 <info@ansibleguy.com>
2025-09-03 15:20:57 +0200

    awx multi support

--- server/src/controllers/awx.controller.v2.js
+++ server/src/controllers/awx.controller.v2.js
@@ -1,36 +1,33 @@
-'use strict';
-import Awx from '../models/awx.model.js';
-import RestResult from '../models/restResult.model.v2.js';
+"use strict";
+import Awx from "../models/awx.model.js";
+import RestResult from "../models/restResult.model.v2.js";
+import Errors from "../lib/errors.js";
 
 // List all AWX or filter by name
 const find = async (req, res) => {
   try {
     if (req.query.name) {
       const awx = await Awx.findByName(req.query.name);
-      if (!awx) {
-        return res.status(404).json(RestResult.error("AWX not found", null));
-      }
       return res.json(RestResult.single(awx));
     } else {
       const awxList = await Awx.findAll();
       return res.json(RestResult.list(awxList));
     }
   } catch (err) {
-    return res.status(500).json(RestResult.error("Failed to find awx", err.toString()));
+    Errors.ReturnError(res, err);
   }
 };
 
 // Create new AWX
 const create = async (req, res) => {
   if (!req.body || Object.keys(req.body).length === 0) {
-    return res.status(400).json(RestResult.error("Please provide all required fields", null));
+    throw new Errors.BadRequestError("Please provide all required fields");
   }
   try {
-    const new_awx = new Awx(req.body);
-    const created = await Awx.create(new_awx);
+    const created = await Awx.create(req.body);
     return res.status(201).json(RestResult.single("awx added", created));
   } catch (err) {
-    return res.status(500).json(RestResult.error("Failed to create awx", err.toString()));
+    Errors.ReturnError(res, err);
   }
 };
 
@@ -39,27 +36,28 @@ const findById = async (req, res) => {
   try {
     const awx = await Awx.findById(req.params.id);
     if (!awx) {
-      return res.status(404).json(RestResult.error("AWX not found", null));
+      throw new Errors.NotFoundError("AWX not found");
     }
+    awx.password = "********"; // mask the password for API
     return res.json(RestResult.single(awx));
   } catch (err) {
-    return res.status(500).json(RestResult.error("Failed to find awx", err.toString()));
+    Errors.ReturnError(res, err);
   }
 };
 
 // Update AWX
 const update = async (req, res) => {
   if (!req.body || Object.keys(req.body).length === 0) {
-    return res.status(400).json(RestResult.error("Please provide all required fields", null));
+    throw new Errors.BadRequestError("Please provide all required fields");
   }
   try {
-    const updated = await Awx.update(new Awx(req.body), req.params.id);
+    const updated = await Awx.update(req.body, req.params.id);
     if (!updated) {
-      return res.status(404).json(RestResult.error("AWX not found", null));
+      throw new Errors.NotFoundError("AWX not found");
     }
     return res.json(RestResult.single(updated));
   } catch (err) {
-    return res.status(500).json(RestResult.error("Failed to update awx", err.toString()));
+    Errors.ReturnError(res, err);
   }
 };
 
@@ -68,11 +66,11 @@ const deleteAwx = async (req, res) => {
   try {
     const deleted = await Awx.delete(req.params.id);
     if (!deleted) {
-      return res.status(404).json(RestResult.error("AWX not found", null));
+      throw new Errors.NotFoundError("AWX not found");
     }
     return res.json(RestResult.single(deleted));
   } catch (err) {
-    return res.status(500).json(RestResult.error("Failed to delete awx", err.toString()));
+    Errors.ReturnError(res, err);
   }
 };
 
@@ -80,14 +78,10 @@ const deleteAwx = async (req, res) => {
 const check = async (req, res) => {
   try {
     const awx = await Awx.findById(req.params.id);
-    if (!awx) {
-      return res.status(404).json(RestResult.error("AWX not found", null));
-    }
-    // You may want to implement a real check/test here:
-    const result = await Awx.checkDecrypted(awx);
-    return res.json(RestResult.single({result: "AWX connection is OK"}));
+    const result = await Awx.check(awx);
+    return res.json(RestResult.single({ result: "AWX connection is OK" }));
   } catch (err) {
-    return res.status(500).json(RestResult.error("Failed to check awx", err.toString()));
+    Errors.ReturnError(res, err);
   }
 };
 
@@ -97,5 +91,5 @@ export default {
   findById,
   update,
   delete: deleteAwx,
-  check
+  check,
 };
--- server/src/models/awx.model.js
+++ server/src/models/awx.model.js
@@ -1,124 +1,64 @@
-'use strict';
+
 import https from 'https';
 import axios from 'axios';
 import logger from '../lib/logger.js';
 import mysql from './db.model.js';
 import crypto from '../lib/crypto.js';
-import NodeCache from 'node-cache';
 import appConfig from '../../config/app.config.js';
+import CrudModel from './crud.model.js';
 
-// we store the awx config for 1 hour (no need to go to database each time)
-const cache = new NodeCache({
-    stdTTL: 3600,
-    checkperiod: (3600 * 0.5)
-});
-const httpsAgent = new https.Agent({
-  rejectUnauthorized: false
-})
-function getHttpsAgent(awxConfig){
-  // logger.debug("config : " + awxConfig)
+function getHttpsAgent(awxConfig) {
   return new https.Agent({
     rejectUnauthorized: !awxConfig.ignore_certs,
     ca: awxConfig.ca_bundle
-  })
+  });
 }
 
-// constructor for awx config
-class Awx {
-  constructor(awx) {
-    if (awx.name !== undefined) this.name = awx.name;
-    if (awx.description !== undefined) this.description = awx.description;
-    if (awx.is_default !== undefined) this.is_default = awx.is_default ? 1 : 0;
-    if (awx.uri !== undefined) this.uri = awx.uri;
-    if (awx.use_credentials !== undefined) this.use_credentials = awx.use_credentials ? 1 : 0;
-    if (awx.username !== undefined) this.username = awx.username;
-    if (awx.password !== undefined) this.password = crypto.encrypt(awx.password);
-    if (awx.token !== undefined) this.token = crypto.encrypt(awx.token);
-    if (awx.ignore_certs !== undefined) this.ignore_certs = awx.ignore_certs ? 1 : 0;
-    if (awx.ca_bundle !== undefined) this.ca_bundle = awx.ca_bundle;
-  }
-  // get the awx config from cache or database (=wrapper function)
-  static async getConfig(name = "") {
-    logger.debug(`Getting AWX config for name: ${name}`);
-    const cacheKey = `awxConfig:${name || "default"}`;
-    let awxConfig = cache.get(cacheKey);
-    if (awxConfig === undefined) {
-      awxConfig = await Awx.findByName(name);
-      cache.set(cacheKey, awxConfig);
-      return awxConfig;
-    } else {
-      logger.debug(`Getting awxConfig for '${name}' from cache`);
-      return awxConfig;
+class Awx extends CrudModel {
+  static modelName = 'awx';
+
+  // Custom preProcess for AWX: unset other defaults if is_default is set
+  static async preProcess(data, action) {
+    if (data.is_default) {
+      logger.info('Unsetting is_default on all other AWX records');
+      await mysql.do('UPDATE AnsibleForms.`awx` SET is_default = 0');
+      // Flush CrudModel cache for awx
+      const cache = this.getCache(this.modelName);
+      if (cache) cache.flushAll();
     }
+    return data;
+  }
+
+  // Proxy CRUD methods to CrudModel, injecting custom preProcess
+  static async create(data) {
+    data = await this.preProcess(data, 'create');
+    return super.create(this.modelName, data);
+  }
+  static async update(data, id) {
+    data = await this.preProcess(data, 'update');
+    return super.update(this.modelName, data, id);
   }
   static async delete(id) {
-    logger.info(`Deleting AWX config with id ${id}`);
-    const res = await mysql.do("DELETE FROM AnsibleForms.`awx` WHERE id = ?", [id]);
-    cache.del("awxConfig");
-    return true;
+    return super.delete(this.modelName, id);
   }
-  static async create(record) {
-    if (!record.name) {
-      throw new Error("Name is required");
-    }
-    logger.info(`Creating AWX config ${record.name}`);
-    // Encrypt sensitive fields before saving
-    if (!record.password) {
-      record.password = "";
-    }
+  static async findById(id) {
+    return super.findById(this.modelName, id);
+  }
+  static async findAll() {
+    return super.findAll(this.modelName);
+  }
+  static async findByName(name) {
+    return super.findByName(this.modelName, name);
+  }
+  static async findByProperty(property, value) {
+    return super.findByProperty(this.modelName, property, value);
+  }
 
-    if (!record.token) {
-      record.token = "";
-    }
-    if (record.is_default) {
-      await Awx.unsetDefault();
-    }
-    const res = await mysql.do("INSERT INTO AnsibleForms.`awx` set ?", record);
-    cache.del("awxConfig");
-    return res.insertId;
-  }
-  static async unsetDefault() {
-    logger.info("Setting is_default=0 on all AWX records");
-    await mysql.do("UPDATE AnsibleForms.`awx` SET is_default = 0");
-    cache.del("awxConfig");
-    return true;
-  }
-  static async update(record, id = 1) {
-    // Find the existing record to get the name (if needed)
-    logger.info(`Updating awx id ${id}`);
-    if (record.is_default) {
-      await Awx.unsetDefault();
-    }
-    await mysql.do("UPDATE AnsibleForms.`awx` set ? WHERE id = ?", [record, id]);
-    cache.del("awxConfig");
-    return record;
-  }
-  static async find() {
-    const result = await mysql.do("SELECT * FROM AnsibleForms.`awx` limit 1;");
-    if (result.length > 0) {
-      try {
-        result[0].token = crypto.decrypt(result[0].token);
-      } catch (e) {
-        // logger.error("Couldn't decrypt awx token, did the secretkey change ?")
-        result[0].token = "";
-      }
-      try {
-        result[0].password = crypto.decrypt(result[0].password);
-      } catch (e) {
-        // logger.error("Couldn't decrypt awx token, did the secretkey change ?")
-        result[0].password = "";
-      }
-      return result[0];
-    } else {
-      logger.error("No awx record in the database, something is wrong");
-      throw "No awx record in the database, something is wrong";
-    }
-  }
+  // AWX-specific logic
   static getAuthorization(awxConfig, encrypted = false) {
-    var axiosConfig = {};
+    let axiosConfig = {};
     if (awxConfig.use_credentials) {
-      // logger.debug('Using credentials and basic authentication')
-      var upw = `${awxConfig.username}:${(encrypted) ? crypto.decrypt(awxConfig.password) : awxConfig.password}`;
+      let upw = `${awxConfig.username}:${encrypted ? crypto.decrypt(awxConfig.password) : awxConfig.password}`;
       axiosConfig = {
         headers: {
           Authorization: `Basic ${Buffer.from(upw).toString('base64')}`
@@ -126,37 +66,17 @@ class Awx {
         httpsAgent: getHttpsAgent(awxConfig)
       };
     } else {
-      // logger.debug('Using token')
       axiosConfig = {
         headers: {
-          Authorization: `Bearer ${(encrypted) ? crypto.decrypt(awxConfig.token) : awxConfig.token}`
+          Authorization: `Bearer ${encrypted ? crypto.decrypt(awxConfig.token) : awxConfig.token}`
         },
         httpsAgent: getHttpsAgent(awxConfig)
       };
     }
     return axiosConfig;
   }
+
   static async check(awxConfig) {
-    // this awxConfig comes from the client, not from the database
-    // add the api prefix to the uri 
-    awxConfig.uri = `${awxConfig.uri}${appConfig.awxApiPrefix}`;
-    logger.info(`Checking AWX connection at ${awxConfig.uri}`);
-    const axiosConfig = Awx.getAuthorization(awxConfig, true);
-    try {
-      const axiosresult = await axios.get(awxConfig.uri + "/job_templates/", axiosConfig);
-      if (axiosresult?.data?.results) {
-        return "Awx Connection is OK";
-      } else {
-        throw new Error("Awx Connection failed");
-      }
-    } catch (e) {
-      logger.error("Error while checking AWX connection", e);
-      throw e;
-    }
-  }
-  static async checkDecrypted(awxConfig) {
-    // this awxConfig comes from the client, not from the database
-    // add the api prefix to the uri 
     awxConfig.uri = `${awxConfig.uri}${appConfig.awxApiPrefix}`;
     logger.info(`Checking AWX connection at ${awxConfig.uri}`);
     const axiosConfig = Awx.getAuthorization(awxConfig, false);
@@ -171,96 +91,7 @@ class Awx {
       logger.error("Error while checking AWX connection", e);
       throw e;
     }
-  }  
-  static async findAll() {
-    logger.info("Finding all AWX records");
-    const sql = "SELECT id, name, description, uri, use_credentials, username, password, token, ignore_certs, ca_bundle, is_default FROM AnsibleForms.`awx`;";
-    const res = await mysql.do(sql, undefined, true);
-    // Decrypt sensitive fields
-    return res.map(record => {
-      if (record.password) {
-        try {
-          record.password = crypto.decrypt(record.password);
-        } catch (e) {
-          logger.error("Failed to decrypt AWX password. Did the secretkey change?");
-          record.password = "";
-        }
-      }
-      if (record.token) {
-        try {
-          record.token = crypto.decrypt(record.token);
-        } catch (e) {
-          logger.error("Failed to decrypt AWX token. Did the secretkey change?");
-          record.token = "";
-        }
-      }
-      return record;
-    });
-  }
-
-  static async findById(id) {
-    logger.info(`Finding AWX by id ${id}`);
-    const sql = "SELECT * FROM AnsibleForms.`awx` WHERE id = ?;";
-    const res = await mysql.do(sql, id);
-    if (res.length > 0) {
-      let record = res[0];
-      if (record.password) {
-        try {
-          record.password = crypto.decrypt(record.password);
-        } catch (e) {
-          logger.error("Failed to decrypt AWX password. Did the secretkey change?");
-          record.password = "";
-        }
-      }
-      if (record.token) {
-        try {
-          record.token = crypto.decrypt(record.token);
-        } catch (e) {
-          logger.error("Failed to decrypt AWX token. Did the secretkey change?");
-          record.token = "";
-        }
-      }
-      return record;
-    } else {
-      throw new Error(`No AWX record found with id ${id}`);
-    }
-  }
-
-  static async findByName(name) {
-    let sql, param;
-    if (!name) {
-      sql = "SELECT * FROM AnsibleForms.`awx` WHERE is_default = 1;";
-      param = [];
-    } else {
-      sql = "SELECT * FROM AnsibleForms.`awx` WHERE name = ?;";
-      param = [name];
-    }
-    const res = await mysql.do(sql, param);
-    if (res.length > 0) {
-      let record = res[0];
-      if (record.password) {
-        try {
-          record.password = crypto.decrypt(record.password);
-        } catch (e) {
-          logger.error("Failed to decrypt AWX password. Did the secretkey change?");
-          record.password = "";
-        }
-      }
-      if (record.token) {
-        try {
-          record.token = crypto.decrypt(record.token);
-        } catch (e) {
-          logger.error("Failed to decrypt AWX token. Did the secretkey change?");
-          record.token = "";
-        }
-      }
-      return record;
-    } else {
-      throw new Error(`No AWX record found with name ${name} and no default found`);
-    }
   }
 }
-
-
 
 export default Awx;
