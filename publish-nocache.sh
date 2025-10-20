b0293b43903d6397c167de3b3b285fc41901326a
ansibleguy76 <info@ansibleguy.com>
2025-10-20 12:56:35 +0200

    move version number to global package.json

--- publish-nocache.sh
+++ publish-nocache.sh
@@ -3,7 +3,7 @@
 docker build --no-cache -t ansibleforms .
 docker tag ansibleforms ansibleguy/ansibleforms
 docker push ansibleguy/ansibleforms
-version=$(jq .version ./client/package.json -r)
+version=$(jq .version ./package.json -r)
 docker tag ansibleforms ansibleguy/ansibleforms:$version
 docker push ansibleguy/ansibleforms:$version
 docker rmi $(docker images -f "dangling=true" -q) -f
