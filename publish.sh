3fbb7e34db388925ba2bb081697f7052e93d30e0
ansibleguy76 <info@ansibleguy.com>
2025-10-20 12:42:29 +0200

    docker build scripts

---
+++ publish.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+docker build -t ansibleforms .
+docker tag ansibleforms ansibleguy/ansibleforms
+docker push ansibleguy/ansibleforms
+version=$(jq .version ./client/package.json -r)
+docker tag ansibleforms ansibleguy/ansibleforms:$version
+docker push ansibleguy/ansibleforms:$version
+docker rmi $(docker images -f "dangling=true" -q) -f
