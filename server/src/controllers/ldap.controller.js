08dad1a1ac9814a31511a9c19bfa7fb1e4705929
ansibleguy76 <info@ansibleguy.com>
2026-02-18 09:50:38 +0100

    new v2

--- server/src/controllers/ldap.controller.js
+++ server/src/controllers/ldap.controller.js
@@ -1,24 +1,43 @@
 'use strict';
 import Ldap from '../models/ldap.model.js';
-import RestResult from '../models/restResult.model.js';
+import RestResultv2 from '../models/restResult.model.v2.js';
+import logger from "../lib/logger.js";
 
-const find = function(req, res) {
-  Ldap.find()
-  .then((ldap)=>{ res.json(new RestResult("success","Ldap found",ldap,"")); })
-  .catch((err) => { res.json(new RestResult("error","Failed to find ldap",null,err.toString())) })
+const find = async function(req, res) {
+  try {
+    const ldap = await Ldap.find();
+    res.status(200).json(RestResultv2.single(ldap));
+  } catch (err) {
+    logger.error("Error finding LDAP: ", err);
+    res.status(500).json(RestResultv2.error("Failed to find ldap", err.toString()));
+  }
 };
-const check = function(req, res) {
-  Ldap.check(new Ldap(req.body))
-  .then(()=>{ res.json(new RestResult("success","Ldap connection ok")) })
-  .catch((err) => { res.json(new RestResult("error","Ldap check failed",null,err.toString())) })
+
+const check = async function(req, res) {
+  if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+    res.status(409).json(RestResultv2.error("No data was sent"));
+    return false;
+  }
+  try {
+    const result = await Ldap.check(new Ldap(req.body));
+    res.status(200).json(RestResultv2.single(result));
+  } catch (err) {
+    logger.error("Error checking LDAP connection: ", err);
+    res.status(500).json(RestResultv2.error("Ldap check failed", err.toString()));
+  }
 };
-const update = function(req, res) {
-  if(req.body.constructor === Object && Object.keys(req.body).length === 0){
-    res.status(400).send({ error:true, message: 'Please provide all required fields' });
-  }else{
-    Ldap.update(new Ldap(req.body))
-    .then(()=>{ res.json(new RestResult("success","Ldap updated",null,"")); })
-    .catch((err) => { res.json(new RestResult("error","Failed to update ldap",null,err.toString())) })
+
+const update = async function(req, res) {
+  if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+    res.status(409).json(RestResultv2.error("No data was sent"));
+    return false;
+  }
+  try {
+    await Ldap.update(new Ldap(req.body));
+    res.status(200).json(RestResultv2.single({ message: "Ldap updated" }));
+  } catch (err) {
+    logger.error("Error updating LDAP: ", err);
+    res.status(500).json(RestResultv2.error("Failed to update ldap", err.toString()));
   }
 };
 
